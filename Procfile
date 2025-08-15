c04f700fd7c0843ab2532356722367f824e369fc
Brian Huang <hibrian1105@gmail.com>
2025-08-15 10:09:01 +0800

    Fix gunicorn deployment issue

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nbcrm.wsgi:application
---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -o errexit
+
+echo "Installing Python dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
+echo "Running database migrations..."
+python manage.py migrate
+
+echo "Build completed successfully!"
